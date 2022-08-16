18f60cd0a52f0c5df61b15cf0c6d4fcbb6c9e737
olamideare <63550820+olamideare@users.noreply.github.com>
2022-08-16 18:01:53 +0100

    1.1

--- index.js
+++ index.js
@@ -5,7 +5,5 @@ document.getElementById("homeScores").textContent = homeScore;
 document.getElementById("guestScore").textContent = guestScore;
 
 function add1Home() {
-  let scorersHome = (homeScore += 1);
-  console.log(scorersHome);
-  homeScore = scorersHome
+  homeScore += 1;
 }
